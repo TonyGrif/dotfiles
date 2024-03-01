4099007365c16d557bce44809faa6971485d3e24
TonyGrif <TonyGriffin2000@gmail.com>
2024-03-01 18:00:10 -0500

    nvim: remove uneeded error message

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,12 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugins",
+  {
+    change_detection = {
+      enabled = false,
+    },
+  })
 
 require("general")
 require("maps")
