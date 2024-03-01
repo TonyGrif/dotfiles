16abbc39a810e7550d775a9d9f4e7be2af2ba519
TonyGrif <TonyGriffin2000@gmail.com>
2024-03-01 18:02:59 -0500

    nvim: install statusline

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,13 +1,3 @@
-if vim.fn.has("nvim-0.8.0") == 0 then
-  vim.api.nvim_echo({
-    { "LazyVim requires Neovim >= 0.8.0\n", "ErrorMsg" },
-    { "Press any key to exit", "MoreMsg" },
-  }, true, {})
-  vim.fn.getchar()
-  vim.cmd([[quit]])
-  return {}
-end
-
 return {
   {
     "catppuccin/nvim",
