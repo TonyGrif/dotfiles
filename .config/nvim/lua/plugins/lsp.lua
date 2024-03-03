16bbae49c691e72d675386e975e1b00848ac7fa3
TonyGrif <TonyGriffin2000@gmail.com>
2024-03-03 01:03:12 -0500

    nvim: add lsp manager and lua lsp functionality

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup {
+      ensure_installed = {
+        "lua_ls",
+      },
+    }
+    require("lspconfig").lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    }
+  end
+}
