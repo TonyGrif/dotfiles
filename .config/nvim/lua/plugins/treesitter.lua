500fc6d7b9378934f6176933ff5f9aea0bfaa6ba
TonyGrif <TonyGriffin2000@gmail.com>
2024-03-19 16:48:24 -0400

    nvim: add kotlin to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,12 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "vimdoc", "python", "lua", "bash" },
+      ensure_installed = {
+        "vimdoc",
+        "python",
+        "kotlin",
+        "lua",
+        "bash" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = false },
